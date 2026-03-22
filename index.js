c48dfe8edf3792c3c83082e86fee2bb239642f12
Dawa Lama <https://github.com/dawa10000>
2026-03-22 13:10:33 +0545

    add

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(fileUpload({
   limits: { fileSize: 5 * 1024 * 1024 }
 }));
 app.use(express.json());
-app.use(express.static('uploads'));
+app.use('/uploads', express.static('uploads'));
 
 // ===== Test Route =====
 app.get('/', (req, res) => {
